785b8e3b7fb7fdf86a6605cc63536e416ce3f7de
Guilherme Rera <guilhermerera@gmail.com>
2024-05-31 09:50:37 -0300

    add: header nav finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
+import { Timer, Scroll } from "phosphor-react";
+
 import { HeaderContainer } from "./styles";
+
+import LogoIgnite from "../../assets/ignite-timer-logo.svg";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
 	return (
 		<HeaderContainer>
-			<span>logo</span>
+			<img src={LogoIgnite} alt='' />
 			<nav>
-				<a href=''>timer</a>
-				<a href=''>history</a>
+				<NavLink to='/' title='Timer'>
+					<Timer size={24} />
+				</NavLink>
+				<NavLink to='history' title='History'>
+					<Scroll size={24} />
+				</NavLink>
 			</nav>
 		</HeaderContainer>
 	);
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,5 +5,33 @@ export const HeaderContainer = styled.header`
 	align-items: center;
 	justify-content: space-between;
 
-    
+	nav {
+		display: flex;
+		gap: 0.5rem;
+
+		a {
+			width: 3rem;
+			height: 3rem;
+
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			color: ${({ theme }) => theme[ "gray-100" ]};
+			border-radius: 4px;
+
+			border-top: 3px solid transparent;
+			border-bottom: 3px solid transparent;
+
+			transition: all 0.1s ease-in-out;
+
+			&:hover {
+				border-bottom-color: ${({ theme }) => theme["green-500"]};
+			}
+
+			&.active {
+				color: ${({ theme }) => theme["green-500"]};
+			}
+		}
+	}
 `;
