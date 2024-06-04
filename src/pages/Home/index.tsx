424ae911be1ac2a2dbed973ac88bef39ce7458ea
Guilherme Rera <guilhermerera@gmail.com>
2024-06-04 00:24:37 -0300

    home page finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
 	CountdownContainer,
 	FormContainer,
 	HomeContainer,
-	Separator
+	MinutesAmountInput,
+	Separator,
+	StartCountdownButton,
+	TaskInput
 } from "./styles";
 
 export function Home() {
@@ -12,9 +15,29 @@ export function Home() {
 			<form action=''>
 				<FormContainer>
 					<label htmlFor='task'>I'll work on</label>
-					<input id='task' type='text' />
+					<TaskInput
+						id='task'
+						type='text'
+						placeholder='Insert a task name to work on'
+						list="task-suggestions"
+					/>
+					<datalist id="task-suggestions">
+						<option value="Project 1" />
+						<option value="Project 2" />
+						<option value="Project 3" />
+						<option value="Project 4" />
+						<option value="Project 5"/>
+
+					</datalist>
 					<label htmlFor='minutesAmount'>for</label>
-					<input id='minutesAmount' type='number' />
+					<MinutesAmountInput
+						id='minutesAmount'
+						type='number'
+						placeholder='00'
+						step={5}
+						min={5}
+						max={60}
+					/>
 					<span>minutes.</span>
 				</FormContainer>
 
@@ -26,10 +49,10 @@ export function Home() {
 					<span>0</span>
 				</CountdownContainer>
 
-				<button type='submit'>
+				<StartCountdownButton type='submit'>
 					<Play size={24} />
 					Start
-				</button>
+				</StartCountdownButton>
 			</form>
 		</HomeContainer>
 	);
