7c8e451f8185911e0b1b545a2e7c9eaa3e4887d7
Guilherme Rera <guilhermerera@gmail.com>
2024-06-17 00:34:52 -0300

    add: countdown stops when hits zero

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -33,6 +33,7 @@ interface Cycle {
 	minutesAmount: number;
 	startDate: Date;
 	interruptedDate?: Date;
+	finishedDate?: Date;
 }
 
 export function Home() {
@@ -66,8 +67,8 @@ export function Home() {
 	}
 
 	function handleInterruptCycle() {
-		setCycles(
-			cycles.map((cycle) => {
+		setCycles((state) =>
+			state.map((cycle) => {
 				if (cycle.id === activeCycleId) {
 					return { ...cycle, interruptedDate: new Date() };
 				} else {
@@ -96,16 +97,34 @@ export function Home() {
 		let interval: number;
 		if (activeCycle) {
 			interval = setInterval(() => {
-				setAmountSecondsPassed(
-					differenceInSeconds(new Date(), activeCycle.startDate)
+				const secondsDifference = differenceInSeconds(
+					new Date(),
+					activeCycle.startDate
 				);
+
+				if (secondsDifference >= totalSeconds) {
+					setCycles((state) =>
+						state.map((cycle) => {
+							if (cycle.id === activeCycleId) {
+								return { ...cycle, finishedDate: new Date() };
+							} else {
+								return cycle;
+							}
+						})
+					);
+
+					setAmountSecondsPassed(totalSeconds);
+					clearInterval(interval);
+				} else {
+					setAmountSecondsPassed(secondsDifference);
+				}
 			}, 1000);
 		}
 
 		return () => {
 			clearInterval(interval);
 		};
-	}, [activeCycle]);
+	}, [activeCycle, totalSeconds, activeCycleId]);
 
 	useEffect(() => {
 		if (activeCycle) {
