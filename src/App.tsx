6e40688f3d8b1459d159bc8f114a7170d5efcb83
Guilherme Rera <guilhermerera@gmail.com>
2024-06-19 02:50:47 -0300

    add: context api with reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
-import { AppRouter } from "./Router";
-
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
-import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
-	return (
-		<ThemeProvider theme={defaultTheme}>
-			<GlobalStyle />
-			<BrowserRouter>
-				<AppRouter />
-			</BrowserRouter>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
